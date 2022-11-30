1e05b7165bbf4a2ae27514390a7eb35464e0580d
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 05:10:09 +0700

    set activityGroup to index

---
+++ src/migrations/20221130211225-index_column_activityGroupId_table_todos.js
@@ -0,0 +1,10 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addIndex('todos', ['activity_group_id']);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex('todos', 'activit_gGrou_iId');
+  },
+};
