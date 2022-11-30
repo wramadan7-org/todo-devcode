2245d8723e1ae1df30a4864553896c6909def8a3
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 03:52:10 +0700

    get all todo route

--- src/routes/todosRoute.js
+++ src/routes/todosRoute.js
@@ -3,6 +3,7 @@ const todosController = require('../controllers/todosController');
 
 const router = Router();
 
-router.post('/', todosController.createTodoes);
+router.post('/', todosController.createTodos);
+router.get('/', todosController.getAllTodos);
 
 module.exports = router;
