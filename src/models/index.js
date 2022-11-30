5c1dfdf9f94bcfb280a5647908e168b54e11810b
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 05:24:16 +0700

    assosiation todos and activities

--- src/models/index.js
+++ src/models/index.js
@@ -1,35 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const { sequelize } = require('../config/database');
+const { Activities } = require('./Activities');
+const { Todos } = require('./Todos');
 
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+const setupSequelizeAssosiation = async () => {
+  Activities.hasMany(Todos, {
+    foreignKey: 'activity_group_id',
   });
+  Todos.hasOne(Activities, {
+    foreignKey: 'id',
+    sourceKey: 'activity_group_id',
+  });
+};
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = setupSequelizeAssosiation;
