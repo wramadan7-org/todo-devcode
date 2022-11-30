0bdf25ffecd8c5e19cdba52683cfdff692939301
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 05:34:43 +0700

    get one todo route

--- src/routes/todosRoute.js
+++ src/routes/todosRoute.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/', todosController.createTodos);
 router.get('/', todosController.getAllTodos);
+router.get('/:idTodo', todosController.getOneTodos);
 
 module.exports = router;
