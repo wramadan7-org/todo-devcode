cb35e67a17648ae60969e9e1dea73b166d5da513
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 07:47:22 +0700

    update path todo route

--- src/routes/todosRoute.js
+++ src/routes/todosRoute.js
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/', todosController.createTodos);
 router.get('/', todosController.getAllTodos);
 router.get('/:idTodo', todosController.getOneTodos);
+router.patch('/:idTodo', todosController.updatePatchTodos);
 
 module.exports = router;
