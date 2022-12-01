79d056120caf9881e3f0148708dd7ce6cd732042
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 20:55:55 +0700

    change variable massage to message

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -10,6 +10,7 @@ const errorConverter = (err, req, res, next) => {
   if (!(error instanceof ApiError)) {
     const statusCode = error.statusCode ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
     const message = error.message || httpStatus[statusCode];
+
     error = new ApiError(message, statusCode, false, err.stack);
   }
   next(error);
@@ -23,19 +24,15 @@ const errorHandler = (err, req, res, next) => {
     message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
   }
 
-  res.locals.errorMassage = err.message;
+  res.locals.errorMessage = err.message;
 
-  const response = {
-    status: statusCode,
-    message,
-    ...(NODE_ENV === 'development' && { stack: err.stack }),
-  };
+  const response = { ...(NODE_ENV === 'development' && { stack: err.stack }) };
 
   if (NODE_ENV === 'development') {
     console.error(err);
   }
 
-  res.sendWrapped(err, response, statusCode);
+  res.sendWrapped(err.message, response, statusCode);
 };
 
 module.exports = {
