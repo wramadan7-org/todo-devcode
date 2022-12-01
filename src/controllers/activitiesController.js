1ebd7821cf62957ead64df3c1ca7f1bdfa7b8372
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 20:56:41 +0700

    use catchAsync

--- src/controllers/activitiesController.js
+++ src/controllers/activitiesController.js
@@ -1,113 +1,90 @@
 const httpStatus = require('http-status');
 const { dataActivities } = require('../helpers/customResponse');
+const catchAsync = require('../helpers/catchAsync');
 const { Activities } = require('../models/Activities');
 
-const createActivities = async (req, res) => {
-  try {
-    const { title, email } = req.body;
+const createActivities = catchAsync(async (req, res) => {
+  const { title, email } = req.body;
 
-    if (!title || title.length === 0) return res.sendWrapped('title cannot be null', {}, httpStatus.BAD_REQUEST);
+  if (!title || title.length === 0) return res.sendWrapped('title cannot be null', {}, httpStatus.BAD_REQUEST);
 
-    const data = { title, email };
+  const data = { title, email };
 
-    const created = await Activities.create(data);
+  const created = await Activities.create(data);
 
-    const response = dataActivities(created);
+  const response = dataActivities(created);
 
-    res.sendWrapped('Success', response, httpStatus.CREATED);
-  } catch (error) {
-    console.log(error);
-    res.send({ status: 500, message: error });
-  }
-};
+  res.sendWrapped('Success', response, httpStatus.CREATED);
+});
 
-const getAllActivities = async (req, res) => {
-  try {
-    const activities = await Activities.findAll();
+const getAllActivities = catchAsync(async (req, res) => {
+  const activities = await Activities.findAll();
 
-    if (!activities) return res.sendWrapped('Fail to get all activities.', [], httpStatus.CONFLICT);
+  if (!activities) return res.sendWrapped('Fail to get all activities.', [], httpStatus.CONFLICT);
 
-    if (activities.length < 0) return res.sendWrapped('Success', activities, httpStatus.OK);
+  if (activities.length < 0) return res.sendWrapped('Success', activities, httpStatus.OK);
 
-    const arrayResponse = [];
+  const arrayResponse = [];
 
-    activities.forEach(async (value) => {
-      const response = dataActivities(value);
+  activities.forEach(async (value) => {
+    const response = dataActivities(value);
 
-      arrayResponse.push(response);
-    });
+    arrayResponse.push(response);
+  });
 
-    res.sendWrapped('Success', arrayResponse, httpStatus.OK);
-  } catch (error) {
-    console.log(error);
-    res.send({ status: 500, message: error });
-  }
-};
+  res.sendWrapped('Success', arrayResponse, httpStatus.OK);
+});
 
-const getOneActivities = async (req, res) => {
+const getOneActivities = catchAsync(async (req, res) => {
   const { idActivities } = req.params;
-  try {
-    const activities = await Activities.findByPk(idActivities);
 
-    if (!activities) return res.sendWrapped(`Activity with ID ${idActivities} Not Found`, {}, httpStatus.NOT_FOUND);
+  const activities = await Activities.findByPk(idActivities);
 
-    const response = dataActivities(activities);
+  if (!activities) return res.sendWrapped(`Activity with ID ${idActivities} Not Found`, {}, httpStatus.NOT_FOUND);
 
-    res.sendWrapped('Success', response, httpStatus.OK);
-  } catch (error) {
-    console.log(error);
-    res.json({ status: 500, message: error });
-  }
-};
+  const response = dataActivities(activities);
 
-const updatePatchActivities = async (req, res) => {
+  res.sendWrapped('Success', response, httpStatus.OK);
+});
+
+const updatePatchActivities = catchAsync(async (req, res) => {
   const { idActivities } = req.params;
   const requestBody = req.body;
 
-  try {
-    const activities = await Activities.findByPk(idActivities);
+  const activities = await Activities.findByPk(idActivities);
 
-    if (!activities) return res.sendWrapped(`Activity with ID ${idActivities} Not Found`, {}, httpStatus.NOT_FOUND);
+  if (!activities) return res.sendWrapped(`Activity with ID ${idActivities} Not Found`, {}, httpStatus.NOT_FOUND);
 
-    if (!requestBody.title || requestBody.title.length === 0) return res.sendWrapped('title cannot be null', {}, httpStatus.BAD_REQUEST);
+  if (!requestBody.title || requestBody.title.length === 0) return res.sendWrapped('title cannot be null', {}, httpStatus.BAD_REQUEST);
 
-    const update = await Activities.update(requestBody, {
-      where: {
-        id: idActivities,
-      },
-    });
+  const update = await Activities.update(requestBody, {
+    where: {
+      id: idActivities,
+    },
+  });
 
-    const afterUpdate = await Activities.findByPk(idActivities);
+  const afterUpdate = await Activities.findByPk(idActivities);
 
-    const response = dataActivities(afterUpdate);
+  const response = dataActivities(afterUpdate);
 
-    res.sendWrapped('Success', response, httpStatus.OK);
-  } catch (error) {
-    console.log(error);
-    res.json({ status: 500, message: error });
-  }
-};
+  res.sendWrapped('Success', response, httpStatus.OK);
+});
 
-const deleteActivies = async (req, res) => {
+const deleteActivies = catchAsync(async (req, res) => {
   const { idActivities } = req.params;
 
-  try {
-    const activities = await Activities.findByPk(idActivities);
+  const activities = await Activities.findByPk(idActivities);
 
-    if (!activities) return res.sendWrapped(`Activity with ID ${idActivities} Not Found`, {}, httpStatus.NOT_FOUND);
+  if (!activities) return res.sendWrapped(`Activity with ID ${idActivities} Not Found`, {}, httpStatus.NOT_FOUND);
 
-    await Activities.destroy({
-      where: {
-        id: idActivities,
-      },
-    });
+  await Activities.destroy({
+    where: {
+      id: idActivities,
+    },
+  });
 
-    res.sendWrapped('Success', {}, httpStatus.OK);
-  } catch (error) {
-    console.log(error);
-    res.json({ satus: 500, message: error });
-  }
-};
+  res.sendWrapped('Success', {}, httpStatus.OK);
+});
 
 module.exports = {
   createActivities,
