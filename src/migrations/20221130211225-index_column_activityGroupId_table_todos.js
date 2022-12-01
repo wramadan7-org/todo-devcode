21e0a76cdd871786a8cf03e0b7dd6ea6dd3f68b6
Wahyu Ramadan <wramadan1203@gmail.com>
2022-12-01 22:58:42 +0700

    fix typo

--- src/migrations/20221130211225-index_column_activityGroupId_table_todos.js
+++ src/migrations/20221130211225-index_column_activityGroupId_table_todos.js
@@ -5,6 +5,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeIndex('todos', 'activit_gGrou_iId');
+    await queryInterface.removeIndex('todos', ['activity_group_id']);
   },
 };
